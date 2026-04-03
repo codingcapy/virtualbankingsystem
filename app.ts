a56d0bf81b84e60966244a1fa0280dba78a231e4
PaulArkhet <spkim0921@gmail.com>
2026-04-03 11:03:27 -0700

    feat: build bun hono server and vite react client

---
+++ app.ts
@@ -0,0 +1,34 @@
+import { serve } from "@hono/node-server";
+import { Hono } from "hono";
+import { serveStatic } from "hono/bun";
+import { cors } from "hono/cors";
+import { logger } from "hono/logger";
+
+const app = new Hono();
+
+app.use("*", logger());
+app.use("*", cors());
+
+const PORT = parseInt(process.env.PORT!) || 3333;
+
+const apiRoutes = app.basePath("/api/v0");
+
+export type ApiRoutes = typeof apiRoutes;
+export default app;
+
+app.use("/*", serveStatic({ root: "./frontend/dist" }));
+app.get("/*", async (c) => {
+  try {
+    const indexHtml = await Bun.file("./frontend/dist/index.html").text();
+    return c.html(indexHtml);
+  } catch (error) {
+    console.error("Error reading index.html:", error);
+    return c.text("Internal Server Error", 500);
+  }
+});
+
+const server = serve({
+  port: PORT,
+  fetch: app.fetch,
+});
+console.log("Server running on port", PORT);
