df2b9907ffaa10696fcc22203b681f20b7a75d6d
PaulArkhet <spkim0921@gmail.com>
2026-04-04 11:20:11 -0700

    feat: build create profile api route

--- routes/profiles.ts
+++ routes/profiles.ts
@@ -1,5 +1,39 @@
+import { zValidator } from "@hono/zod-validator";
+import { createInsertSchema } from "drizzle-zod";
 import { Hono } from "hono";
+import z from "zod";
+import { profiles as profilesTable } from "../schemas/profiles";
+import { mightFail } from "might-fail";
+import { db } from "../db";
+import { randomUUIDv7 } from "bun";
+import { HTTPException } from "hono/http-exception";
 
-export const profilesRouter = new Hono().post("/", async (c) => {
-  return c.json({});
-});
+export const profilesRouter = new Hono().post(
+  "/",
+  zValidator(
+    "json",
+    createInsertSchema(profilesTable).omit({
+      profileId: true,
+      role: true,
+      status: true,
+      createdAt: true,
+      updatedAt: true,
+    }),
+  ),
+  async (c) => {
+    const insertValues = c.req.valid("json");
+    const { result: profileInsertResult, error: profileInsertError } =
+      await mightFail(
+        db
+          .insert(profilesTable)
+          .values({ profileId: randomUUIDv7(), ...insertValues })
+          .returning(),
+      );
+    if (profileInsertError)
+      throw new HTTPException(500, {
+        message: "Error while creating profile",
+        cause: profileInsertError,
+      });
+    return c.json({ profile: profileInsertResult[0] });
+  },
+);
